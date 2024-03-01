beb83d1c3ee4f1c2101c2e5482c7b5708541b7ab
William Fox <bcf7023@rit.edu>
2024-03-01 09:47:40 -0500

    How do I make this wait for requests properly?

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -2,21 +2,21 @@ const ultraBallURL = '../Images/UltraBall.png';
 const fs = require('fs');
 
 const getUltraBall = (request, response) => {
-    fs.readFile(ultraBallURL, (err, data) => {
-        if (err) {
-            console.error(err);
-            response.writeHead(500, { 'Content-Type': 'text/plain' });
-            response.end('Internal Server Error');
-        } else {
-            const contentType = 'image/png'; // Change this to 'image/png' if your image is in PNG format
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(data);
-        }
-    });
+  fs.readFile(ultraBallURL, (err, data) => {
+    if (err) {
+      console.error(err);
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.end('Internal Server Error');
+    } else {
+      const contentType = 'image/png'; // Change this to 'image/png' if your image is in PNG format
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(data);
+    }
+  });
 };
 
 module.exports = {
-    getUltraBall
+  getUltraBall,
 };
 
 // The Totem Lurantis is not hard.
